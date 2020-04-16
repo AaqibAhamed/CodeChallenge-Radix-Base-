abff80c8f39dae998acd425f525011ae5d32ec9d
Ahamed Aaqib <ahamedaaqib96@outlook.com>
2020-04-16 11:39:39 +0530

    Add project files.

---
+++ CodeChallenge_Radix(Base)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge_Radix_Base_
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int reminder;
+            var numberList = new List<int>();
+
+            while (n>2)
+            {
+                reminder = n % 2;
+                n = n / 2;
+                numberList.Add(reminder);
+
+            }
+        }
+    }
+}
